44f93beab53e50697410ac8417da32b4ddd45088
a139954 <a139954@agl.com.au>
2019-05-27 12:51:13 +1000

    Add unit test for controller

--- AGL.PetOwner.API.Tests/Controllers/PetOwnerControllerTest.cs
+++ AGL.PetOwner.API.Tests/Controllers/PetOwnerControllerTest.cs
@@ -5,6 +5,8 @@ using AGL.PetOwner.API.Controllers;
 using AGL.PetOwner.BusinessLogic;
 using NSubstitute;
 using System.Net.Http;
+using AGL.PetOwner.API.Tests.TestData;
+using System.Web.Http;
 
 namespace AGL.PetOwner.API.Tests.Controllers
 {
@@ -16,5 +18,21 @@ namespace AGL.PetOwner.API.Tests.Controllers
         {
             petOwnerService = Substitute.For<IPetOwnerService>();
         }
+
+        [Fact]
+        public void GetPetsByOwnerGenderNotNull()
+        {
+            // Arrange
+            var mockProvider = new PetOwnerTestData();
+            var mockRepository = Substitute.For<PetOwner.BusinessLogic.IPetOwnerService>();
+            mockRepository.GetPetOwnerDetails("cat").Returns(mockProvider.GetMockPetsByOwnerGender());
+            var controller = new PetOwnerController(petOwnerService);
+
+            // Act
+            var result = controller.GetPetsByOwnerGender();
+
+            // Assert
+            AssertLibrary.Assert.IsNotNull(result);
+        } 
     }
 }
